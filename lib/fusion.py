991d5274f742edad205ae1fa8cdbf516b496bfde
Brent Yi <yibrenth@gmail.com>
2020-02-23 22:11:41 -0800

    Add fusion softmax

--- lib/fusion.py
+++ lib/fusion.py
@@ -9,7 +9,8 @@ import torch.optim as optim
 
 class KalmanFusionModel(nn.Module):
 
-    def __init__(self, image_model, force_model, weight_model, fusion_type="weighted"):
+    def __init__(self, image_model, force_model,
+                 weight_model, fusion_type="weighted"):
         super().__init__()
 
         self.image_model = image_model
@@ -40,16 +41,28 @@ class KalmanFusionModel(nn.Module):
 
             force_beta, image_beta, _ = self.weight_model.forward(observations)
             weights = [image_beta, force_beta]
-            sigma_weights = [torch.diag_embed(image_beta, offset=0, dim1=-2, dim2=-1), torch.diag_embed(force_beta, offset=0, dim1=-2, dim2=-1)]
+            sigma_weights = [
+                torch.diag_embed(
+                    image_beta,
+                    offset=0,
+                    dim1=-2,
+                    dim2=-1),
+                torch.diag_embed(
+                    force_beta,
+                    offset=0,
+                    dim1=-2,
+                    dim2=-1)]
             states_pred = [image_state, force_state]
             state_sigma_pred = [image_state_sigma, force_state_sigma]
 
             if self.fusion_type == "weighted":
                 state = self.weighted_average(states_pred, weights)
-                state_sigma = self.weighted_average(state_sigma_pred, sigma_weights)
+                state_sigma = self.weighted_average(
+                    state_sigma_pred, sigma_weights)
             elif self.fusion_type == "poe":
                 state = self.product_of_experts(states_pred, weights)
-                state_sigma = self.weighted_average(state_sigma_pred, sigma_weights)
+                state_sigma = self.weighted_average(
+                    state_sigma_pred, sigma_weights)
 
             #todo: sigma weighting and poe -- make sure you divide
 
@@ -63,7 +76,7 @@ class KalmanFusionModel(nn.Module):
         epsilon = 0.0001
         weights = weights / (torch.sum(weights, dim=0) + epsilon)
 
-        weighted_average = torch.sum(weights*predictions, dim=0)
+        weighted_average = torch.sum(weights * predictions, dim=0)
 
         # print("pred: ", predictions)
         # print("weights" , weights)
@@ -74,28 +87,42 @@ class KalmanFusionModel(nn.Module):
         assert len(predictions) == len(weights)
         weights = torch.stack(weights)
         predictions = torch.stack(predictions)
-        T= 1.0/weights
-        mu = (predictions * T).sum(0) * (1.0/ T.sum(0))
-        var = (1.0/T.sum(0))
+        T = 1.0 / weights
+        mu = (predictions * T).sum(0) * (1.0 / T.sum(0))
+        var = (1.0 / T.sum(0))
 
         return mu, var
 
+
 class CrossModalWeights(nn.Module):
 
-    def __init__(self, state_dim=2, units=32):
+    def __init__(self, state_dim=2, units=32, use_softmax=False):
         super().__init__()
 
         obs_pose_dim = 3
         obs_sensors_dim = 7
         self.state_dim = state_dim
+        self.use_softmax = use_softmax
 
         self.observation_image_layers = nn.Sequential(
-            nn.Conv2d(in_channels=1, out_channels=32, kernel_size=5, padding=2),
+            nn.Conv2d(
+                in_channels=1,
+                out_channels=32,
+                kernel_size=5,
+                padding=2),
             nn.ReLU(inplace=True),
             resblocks.Conv2d(channels=32, kernel_size=3),
-            nn.Conv2d(in_channels=32, out_channels=16, kernel_size=3, padding=1),
+            nn.Conv2d(
+                in_channels=32,
+                out_channels=16,
+                kernel_size=3,
+                padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels=16, out_channels=2, kernel_size=3, padding=1),
+            nn.Conv2d(
+                in_channels=16,
+                out_channels=2,
+                kernel_size=3,
+                padding=1),
             nn.Flatten(),  # 32 * 32 * 8
             nn.Linear(2 * 32 * 32, units),
             nn.ReLU(inplace=True),
@@ -110,40 +137,50 @@ class CrossModalWeights(nn.Module):
             resblocks.Linear(units, activation='leaky_relu'),
         )
 
-        self.shared_layers = nn.Sequential(
-            nn.Linear(units * 3, units * 3),
-            nn.ReLU(inplace=True),
-            resblocks.Linear(3 * units),
-        )
+        if self.use_softmax:
+            self.shared_layer = nn.Sequential(
+                nn.Linear(units * 3, units),
+                nn.ReLu(inplace=True),
+                resblocks.Linear(units, units),
+                resblocks.Linear(units, units),
+                resblocks.Linear(units, units),
+                nn.Linear(units, 2 * self.state_dim),
+            )
+        else:
+            self.shared_layers = nn.Sequential(
+                nn.Linear(units * 3, units * 3),
+                nn.ReLU(inplace=True),
+                resblocks.Linear(3 * units),
+            )
 
-        self.force_prop_layer = nn.Sequential(
-            nn.Linear(units, units),
-            nn.ReLU(inplace=True),
-            nn.Linear(units, self.state_dim),
-            nn.Sigmoid(),
-        )
+            self.force_prop_layer = nn.Sequential(
+                nn.Linear(units, units),
+                nn.ReLU(inplace=True),
+                nn.Linear(units, self.state_dim),
+                nn.Sigmoid(),
+            )
 
-        self.image_prop_layer = nn.Sequential(
-            nn.Linear(units, units),
-            nn.ReLU(inplace=True),
-            nn.Linear(units, self.state_dim),
-            nn.Sigmoid(),
-        )
+            self.image_prop_layer = nn.Sequential(
+                nn.Linear(units, units),
+                nn.ReLU(inplace=True),
+                nn.Linear(units, self.state_dim),
+                nn.Sigmoid(),
+            )
 
-        self.fusion_layer = nn.Sequential(
-            nn.Linear(units, units),
-            nn.ReLU(inplace=True),
-            nn.Linear(units, self.state_dim),
-            nn.Sigmoid(),
-        )
+            self.fusion_layer = nn.Sequential(
+                nn.Linear(units, units),
+                nn.ReLU(inplace=True),
+                nn.Linear(units, self.state_dim),
+                nn.Sigmoid(),
+            )
 
         for m in self.modules():
             if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
                 nn.init.kaiming_normal_(m.weight.data)
                 if m.bias is not None:
                     m.bias.data.zero_()
-            elif isinstance(m, nn.BatchNorm2d):
-                m.weight.data.fill_(1)
+                elif isinstance(m, nn.BatchNorm2d):
+                    m.weight.data.fill_(1)
                 m.bias.data.zero_()
 
         self.units = units
@@ -167,14 +204,20 @@ class CrossModalWeights(nn.Module):
         assert observation_features.shape == (N, self.units * 3)
 
         shared_features = self.shared_layers(observation_features)
-        assert shared_features.shape == (N, self.units * 3)
 
-        force_prop_beta = self.force_prop_layer(shared_features[:, :self.units])
-        image_beta = self.image_prop_layer(shared_features[:, self.units:self.units * 2])
-        fusion_beta = self.fusion_layer(shared_features[:, self.units * 2:])
+        if self.use_softmax:
+            assert shared_features.shape == (N, 2 * self.state_dim)
+            log_softmax = F.log_softmax(
+                shared_features.reshape((N, 2, self.state_dim)),
+                dim=1
+            )
+            force_prop_beta = log_softmax[:, 0, :]
+            image_beta = log_softmax[:, 1, :]
+        else:
+            assert shared_features.shape == (N, self.units * 3)
+            force_prop_beta = self.force_prop_layer(
+                shared_features[:, :self.units])
+            image_beta = self.image_prop_layer(
+                shared_features[:, self.units:self.units * 2])
 
-        return  image_beta,force_prop_beta, fusion_beta
-
-
-
-
+        return image_beta, force_prop_beta
