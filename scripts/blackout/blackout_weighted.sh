5b4d1c669a663760814ff81e10278cdaab0247b3
Michelle Lee <michellelee@bohg-ws-2.stanford.edu>
2020-02-26 23:51:05 -0800

    bash script update

--- scripts/blackout/blackout_weighted.sh
+++ scripts/blackout/blackout_weighted.sh
@@ -5,28 +5,31 @@ then
 	echo "Please provide exactly 1 argument blackout amount"
 else
 	ratio=$1
+	echo $ratio 
+	name="fusion_weighted_blackout_"$ratio"_0"
+	echo $name
 
 	python train_fusion.py --data_size 1000 --batch 128 --epochs 1 --fusion_type weighted \
-	--experiment_name fusion_weighted_blackout_$ratio_0 --pretrain 5 --blackout $ratio \
+	--experiment_name $name --pretrain 5 --blackout $ratio \
 	 --lr 1e-5 
 
 	python train_fusion.py --data_size 1000 --batch 128 --epochs 4 --fusion_type weighted \
-	--experiment_name fusion_weighted_blackout_$ratio_0 --pretrain 5 --blackout $ratio \
+	--experiment_name $name --pretrain 5 --blackout $ratio \
 	--train fusion  --lr 1e-5 
 
 	python train_fusion.py --data_size 1000 --batch 128 --epochs 5 --fusion_type weighted \
-	--experiment_name fusion_weighted_blackout_$ratio_0 --pretrain 5 --blackout $ratio \
+	--experiment_name $name --pretrain 5 --blackout $ratio \
 	--train fusion  --lr 1e-5 
 
 
 	python train_fusion.py --data_size 1000 --batch 128 --epochs 5 --fusion_type weighted \
-	--experiment_name fusion_weighted_blackout_$ratio_0 --pretrain 5 --blackout $ratio \
+	--experiment_name $name --pretrain 5 --blackout $ratio \
 	--train fusion  --lr 1e-5\
 	--init_state_noise 0.3 
 
 
 	python train_fusion.py --data_size 1000 --batch 128 --epochs 5 --fusion_type weighted \
-	--experiment_name fusion_weighted_blackout_$ratio_0 --pretrain 5 --blackout $ratio \
+	--experiment_name $name --pretrain 5 --blackout $ratio \
 	--train fusion  --lr 1e-5\
 	--init_state_noise 0.4 
 
