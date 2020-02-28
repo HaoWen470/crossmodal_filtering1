25fb181e1660640e23d3fcd63683e96abd5d5341
Michelle Lee <michellelee@bohg-ws-2.stanford.edu>
2020-02-28 02:44:17 -0800

    sequential rate training scripts

---
+++ scripts/blackout/sequential_rate/seq_weighted.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+
+name="fusion_weighted_seq5_0"
+echo $name
+
+python train_fusion.py --data_size 1000 --batch 128 --epochs 1 --fusion_type weighted \
+--experiment_name $name --pretrain 5  --sequential_image 5  \
+--lr 1e-5 
+
+python train_fusion.py --data_size 1000 --batch 128 --epochs 4 --fusion_type weighted \
+--experiment_name $name --pretrain 5  --sequential_image 5 \
+--lr 1e-5 	--train fusion 
+
+python train_fusion.py --data_size 1000 --batch 128 --epochs 5 --fusion_type weighted \
+--experiment_name $name --pretrain 5  --sequential_image 5 \
+--lr 1e-5 	--train fusion 
+
+	python train_fusion.py --data_size 1000 --batch 128 --epochs 5 --fusion_type weighted \
+--experiment_name $name --pretrain 5  --sequential_image 5 \
+--lr 1e-5 	--train fusion 
+
+
+# python train_fusion.py --data_size 1000 --batch 128 --epochs 5 --fusion_type weighted \
+# --experiment_name $name --pretrain 5  --sequential_image 5 \
+# --train fusion  --lr 1e-5 \
+# --init_state_noise 0.3 
+
+
+# python train_fusion.py --data_size 1000 --batch 128 --epochs 5 --fusion_type weighted \
+# --experiment_name $name --pretrain 5  --sequential_image 5 \
+# --train fusion  --lr 1e-5 \
+# --init_state_noise 0.4 
+
+
+
