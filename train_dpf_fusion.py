cf5dd28e91359e2f24b69f6dda5a3f3317843bf0
Brent Yi <yibrenth@gmail.com>
2020-02-28 19:25:39 -0800

    Uncomment DPF fusion training

--- train_dpf_fusion.py
+++ train_dpf_fusion.py
@@ -139,117 +139,117 @@ elif args.dataset == "omnipush":
         **dataset_args
     )
 
-# # Pre-train dynamics
-# models = [
-#     (pf_image_model, 'dynamics_image'),
-#     #     (pf_force_model, 'dynamics_force'),
-# ]
-# dataloader = torch.utils.data.DataLoader(
-#     dynamics_trainset,
-#     batch_size=32,
-#     shuffle=True,
-#     num_workers=2)
-# for pf_model, optim_name in models:
-#     for i in range(DYNAMICS_PRETRAIN_EPOCHS):
-#         print("Pre-training dynamics epoch", i)
-#         panda_training.train_dynamics(
-#             buddy,
-#             pf_model,
-#             dataloader,
-#             log_interval=1,
-#             optim_name=optim_name)
-# buddy.save_checkpoint()
-# buddy.load_checkpoint_module(
-#     source='image_model.dynamics_model',
-#     target='force_model.dynamics_model')
-# buddy.save_checkpoint("phase_0_dynamics_pretrain")
-#
-# # Pre-train dynamics (recurrent)
-# models = [
-#     (pf_image_model, 'dynamics_recurrent_image'),
-#     #     (pf_force_model, 'dynamics_recurrent_force'),
-# ]
-# dataloader = torch.utils.data.DataLoader(
-#     dynamics_recurrent_trainset,
-#     batch_size=32,
-#     shuffle=True,
-#     num_workers=2)
-# for pf_model, optim_name in models:
-#     for i in range(DYNAMICS_RECURRENT_PRETRAIN_EPOCHS):
-#         print("Pre-training dynamics recurrent epoch", i)
-#         panda_training.train_dynamics_recurrent(
-#             buddy,
-#             pf_model,
-#             dataloader,
-#             log_interval=1,
-#             loss_type='l2',
-#             optim_name=optim_name)
-# buddy.save_checkpoint()
-# buddy.load_checkpoint_module(
-#     source='image_model.dynamics_model',
-#     target='force_model.dynamics_model')
-# buddy.save_checkpoint("phase_1_dynamics_pretrain_recurrent")
-# buddy.save_checkpoint()
-#
-# # Pre-train measurement
-# models = [
-#     (pf_image_model, 'measurement_image'),
-#     (pf_force_model, 'measurement_force'),
-# ]
-# measurement_trainset_loader = torch.utils.data.DataLoader(
-#     measurement_trainset,
-#     batch_size=32,
-#     shuffle=True,
-#     num_workers=16)
-# for pf_model, optim_name in models:
-#     for i in range(MEASUREMENT_PRETRAIN_EPOCHS):
-#         print("Pre-training measurement epoch", i)
-#         panda_training.train_measurement(
-#             buddy,
-#             pf_model,
-#             measurement_trainset_loader,
-#             log_interval=20,
-#             optim_name=optim_name)
-# buddy.save_checkpoint("phase_2_measurement_pretrain")
-# buddy.save_checkpoint()
-#
-# # E2E train (individual)
-# models = [
-#     (pf_image_model, 'e2e_image'),
-#     (pf_force_model, 'e2e_force'),
-# ]
-# for pf_model, optim_name in models:
-#     pf_model.freeze_measurement_model = False
-#     pf_model.freeze_dynamics_model = True
-#     e2e_trainset_loader = torch.utils.data.DataLoader(
-#         e2e_trainset, batch_size=32, shuffle=True, num_workers=2)
-#     for i in range(E2E_INDIVIDUAL_EPOCHS):
-#         print(f"E2E individual training epoch {optim_name}", i)
-#         panda_training.train_e2e(
-#             buddy,
-#             pf_model,
-#             e2e_trainset_loader,
-#             loss_type="mse",
-#             optim_name=optim_name)
-# buddy.save_checkpoint("phase_3_e2e_individual")
-# buddy.save_checkpoint()
-#
-# # E2E train (joint)
-# optim_name = "e2e_fusion"
-# pf_fusion_model.freeze_image_model = True
-# pf_fusion_model.freeze_force_model = True
-# e2e_trainset_loader = torch.utils.data.DataLoader(
-#     e2e_trainset, batch_size=32, shuffle=True, num_workers=2)
-# for i in range(E2E_JOINT_EPOCHS):
-#     print("Training E2E (joint) epoch", i)
-#     panda_training.train_e2e(
-#         buddy,
-#         pf_fusion_model,
-#         e2e_trainset_loader,
-#         loss_type="mse",
-#         optim_name=optim_name)
-# buddy.save_checkpoint("phase_4_e2e_joint")
-# buddy.save_checkpoint()
+# Pre-train dynamics
+models = [
+    (pf_image_model, 'dynamics_image'),
+    #     (pf_force_model, 'dynamics_force'),
+]
+dataloader = torch.utils.data.DataLoader(
+    dynamics_trainset,
+    batch_size=32,
+    shuffle=True,
+    num_workers=2)
+for pf_model, optim_name in models:
+    for i in range(DYNAMICS_PRETRAIN_EPOCHS):
+        print("Pre-training dynamics epoch", i)
+        panda_training.train_dynamics(
+            buddy,
+            pf_model,
+            dataloader,
+            log_interval=1,
+            optim_name=optim_name)
+buddy.save_checkpoint()
+buddy.load_checkpoint_module(
+    source='image_model.dynamics_model',
+    target='force_model.dynamics_model')
+buddy.save_checkpoint("phase_0_dynamics_pretrain")
+
+# Pre-train dynamics (recurrent)
+models = [
+    (pf_image_model, 'dynamics_recurrent_image'),
+    #     (pf_force_model, 'dynamics_recurrent_force'),
+]
+dataloader = torch.utils.data.DataLoader(
+    dynamics_recurrent_trainset,
+    batch_size=32,
+    shuffle=True,
+    num_workers=2)
+for pf_model, optim_name in models:
+    for i in range(DYNAMICS_RECURRENT_PRETRAIN_EPOCHS):
+        print("Pre-training dynamics recurrent epoch", i)
+        panda_training.train_dynamics_recurrent(
+            buddy,
+            pf_model,
+            dataloader,
+            log_interval=1,
+            loss_type='l2',
+            optim_name=optim_name)
+buddy.save_checkpoint()
+buddy.load_checkpoint_module(
+    source='image_model.dynamics_model',
+    target='force_model.dynamics_model')
+buddy.save_checkpoint("phase_1_dynamics_pretrain_recurrent")
+buddy.save_checkpoint()
+
+# Pre-train measurement
+models = [
+    (pf_image_model, 'measurement_image'),
+    (pf_force_model, 'measurement_force'),
+]
+measurement_trainset_loader = torch.utils.data.DataLoader(
+    measurement_trainset,
+    batch_size=32,
+    shuffle=True,
+    num_workers=16)
+for pf_model, optim_name in models:
+    for i in range(MEASUREMENT_PRETRAIN_EPOCHS):
+        print("Pre-training measurement epoch", i)
+        panda_training.train_measurement(
+            buddy,
+            pf_model,
+            measurement_trainset_loader,
+            log_interval=20,
+            optim_name=optim_name)
+buddy.save_checkpoint("phase_2_measurement_pretrain")
+buddy.save_checkpoint()
+
+# E2E train (individual)
+models = [
+    (pf_image_model, 'e2e_image'),
+    (pf_force_model, 'e2e_force'),
+]
+for pf_model, optim_name in models:
+    pf_model.freeze_measurement_model = False
+    pf_model.freeze_dynamics_model = True
+    e2e_trainset_loader = torch.utils.data.DataLoader(
+        e2e_trainset, batch_size=32, shuffle=True, num_workers=2)
+    for i in range(E2E_INDIVIDUAL_EPOCHS):
+        print(f"E2E individual training epoch {optim_name}", i)
+        panda_training.train_e2e(
+            buddy,
+            pf_model,
+            e2e_trainset_loader,
+            loss_type="mse",
+            optim_name=optim_name)
+buddy.save_checkpoint("phase_3_e2e_individual")
+buddy.save_checkpoint()
+
+# E2E train (joint)
+optim_name = "e2e_fusion"
+pf_fusion_model.freeze_image_model = True
+pf_fusion_model.freeze_force_model = True
+e2e_trainset_loader = torch.utils.data.DataLoader(
+    e2e_trainset, batch_size=32, shuffle=True, num_workers=2)
+for i in range(E2E_JOINT_EPOCHS):
+    print("Training E2E (joint) epoch", i)
+    panda_training.train_e2e(
+        buddy,
+        pf_fusion_model,
+        e2e_trainset_loader,
+        loss_type="mse",
+        optim_name=optim_name)
+buddy.save_checkpoint("phase_4_e2e_joint")
+buddy.save_checkpoint()
 
 # E2E train (joint)
 optim_name = "e2e_fusion"
