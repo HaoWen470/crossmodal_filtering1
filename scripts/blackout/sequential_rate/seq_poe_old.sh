3c850c494be6de9b9cd5f5ba6e7e2fba2169f477
Michelle Lee <michellelee@bohg-ws-2.stanford.edu>
2020-02-29 16:36:36 -0800

    oldpoe

---
+++ scripts/blackout/sequential_rate/seq_poe_old.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+
+load="checkpoints/fusion_poe_blackout_0.8_1-phase_3_e2e.ckpt"
+name="fusion_poe_old_seq5_0"
+echo $name
+
+python train_fusion.py --data_size 1000 --batch 128 --epochs 5 --fusion_type poe \
+--experiment_name $name --pretrain 5  --sequential_image 5 \
+--lr 1e-5 	--train fusion --load_checkpoint load --old_weighting
+
+python train_fusion.py --data_size 1000 --batch 128 --epochs 5 --fusion_type poe \
+--experiment_name $name --pretrain 5  --sequential_image 5 \
+--lr 1e-5 	--train fusion --old_weighting
+
+	python train_fusion.py --data_size 1000 --batch 128 --epochs 5 --fusion_type poe \
+--experiment_name $name --pretrain 5  --sequential_image 5 \
+--lr 1e-5 	--train fusion --old_weighting
+
+
+
