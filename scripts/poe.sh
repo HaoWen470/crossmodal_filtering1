49ca7c4db30d5e4c9aae585751a355027bb0bfeb
Michelle Lee <michellelee@bohg-ws-2.stanford.edu>
2020-02-29 16:12:11 -0800

    many loss scripts

--- scripts/poe.sh
+++ scripts/poe.sh
@@ -16,20 +16,33 @@
 
 
 
-python train_fusion.py --data_size 1000 --batch 128 --epochs 1 --fusion_type poe \
---experiment_name fusion_poe_truncated_0 --pretrain 5 \
---lr 1e-5 --start_timestep 200
+# python train_fusion.py --data_size 1000 --batch 128 --epochs 1 --fusion_type poe \
+# --experiment_name fusion_poe_truncated_0 --pretrain 5 \
+# --lr 1e-5 --start_timestep 200
 
-python train_fusion.py --data_size 1000 --batch 128 --epochs 4 --fusion_type poe \
---experiment_name fusion_poe_truncated_0 --pretrain 5 \
---train fusion --lr 1e-5 --start_timestep 200
+# python train_fusion.py --data_size 1000 --batch 128 --epochs 4 --fusion_type poe \
+# --experiment_name fusion_poe_truncated_0 --pretrain 5 \
+# --train fusion --lr 1e-5 --start_timestep 200
 
+
+# python train_fusion.py --data_size 1000 --batch 128 --epochs 5 --fusion_type poe \
+# --experiment_name fusion_poe_truncated_0 --pretrain 5 \
+# --train fusion  --lr 1e-5 --start_timestep 200
+
+
+# python train_fusion.py --data_size 1000 --batch 128 --epochs 5 --fusion_type poe \
+# --experiment_name fusion_poe_truncated_0 --pretrain 5 \
+# --train fusion  --lr 1e-5 --start_timestep 200
 
 python train_fusion.py --data_size 1000 --batch 128 --epochs 5 --fusion_type poe \
---experiment_name fusion_poe_truncated_0 --pretrain 5 \
---train fusion  --lr 1e-5 --start_timestep 200
-
+--experiment_name fusion_poe_many_loss --pretrain 10 \
+--load_checkpoint checkpoints/fusion_poe_1000_hu64_1loss-phase_3_e2e.ckpt \
+--lr 1e-5 --train fusion --many_loss 
 
 python train_fusion.py --data_size 1000 --batch 128 --epochs 5 --fusion_type poe \
---experiment_name fusion_poe_truncated_0 --pretrain 5 \
---train fusion  --lr 1e-5 --start_timestep 200
+--experiment_name fusion_poe_final_1 --pretrain 5 \
+--train fusion  --lr 1e-5
+
+python train_fusion.py --data_size 1000 --batch 128 --epochs 5 --fusion_type poe \
+--experiment_name fusion_poe_final_1 --pretrain 5 \
+--train fusion  --lr 1e-5 \
